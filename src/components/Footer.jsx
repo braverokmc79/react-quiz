3d4598c43b0bdf1d7db866f79ab50e902811b898
braverokmc79 <braverokmc79@gmail.com>
2024-03-20 12:32:55 +0900

    css update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <div>
+        
             <a href="https://macaronics.net/" target="_blank">macaronics.net</a>
-        </div>
+        
     </footer>
   )
 }
